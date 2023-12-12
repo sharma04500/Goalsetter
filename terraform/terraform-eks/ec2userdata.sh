72f98ba75924bec7c5e0e0aaa8dcc012d5bd9c43
sharma <iluvcinemaofficial@gmail.com>
2023-12-13 00:21:03 +0530

    terraform_destroy

--- terraform/terraform-eks/ec2userdata.sh
+++ terraform/terraform-eks/ec2userdata.sh
@@ -15,3 +15,12 @@ apt update && apt install -y nodejs kubectl
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+# Installing eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
